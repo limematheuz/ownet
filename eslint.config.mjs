d762c4731ef1019ee6bcd856153a0ade12c25b9d
limematheuz <mribeiro17.info@gmail.com>
2025-01-30 23:53:36 +0100

    Fix ESLint config for Vercel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,10 +13,10 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off', // Desactiva esta regla
-      '@typescript-eslint/no-unused-vars': 'off', // Desactiva esta regla
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
 ];
 
-export { eslintConfig };
+export default eslintConfig;
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,24 +4,26 @@ import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
 import "./globals.css";
 
+type Locales = "en" | "ge" | "fr" | "es";
+
 export default async function LocaleLayout({
   children,
   params
 }: {
   children: React.ReactNode;
-  params: {locale: string};
+  params: {locale: Locales};
 }) {
-  const { locale } = await params;
+  const { locale } = params;
 
-  if (!routing.locales.includes(locale as any)) {
+  if (!routing.locales.includes(locale)) {
     notFound();
   }
  
   const messages = await getMessages({ locale });
  
   return (
-    <html lang={locale} >
-      <body className="max-w-screen" >
+    <html lang={locale}>
+      <body className="max-w-screen">
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
