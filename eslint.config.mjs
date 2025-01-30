49b98d92736454fdb15e2ff53d454f8502bdecbc
limematheuz <mribeiro17.info@gmail.com>
2025-01-30 23:47:00 +0100

    Fix ESLint config for Vercel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,20 +9,20 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ✅ Ignora ESLint en el build de Vercel
-  },
-};
-
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off', // Desactiva esta regla
+      '@typescript-eslint/no-unused-vars': 'off', // Desactiva esta regla
     },
   },
 ];
 
-export default {eslintConfig, nextConfig};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // Ignora ESLint durante el build en Vercel
+  },
+};
+
+export { eslintConfig, nextConfig };
