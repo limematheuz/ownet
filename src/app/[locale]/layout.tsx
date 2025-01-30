7049c8627cd82d52d8e1b330089218859bc66ccc
limematheuz <mribeiro17.info@gmail.com>
2025-01-31 00:20:11 +0100

    Fix ESLint config for Vercel

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,18 +1,18 @@
-import {NextIntlClientProvider} from 'next-intl';
-import {getMessages} from 'next-intl/server';
-import {notFound} from 'next/navigation';
-import {routing} from '@/i18n/routing';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+import { routing } from '@/i18n/routing';
 import "./globals.css";
 
 type Locales = "en" | "ge" | "fr" | "es";
 
 interface LayoutProps {
   children: React.ReactNode;
-  params: { locale: Locales };
+  params: Promise<{ locale: Locales }>;  // Asegúrate de que `params` sea un `Promise`
 }
 
 export default async function LocaleLayout({ children, params }: LayoutProps) {
-  const { locale } = params;
+  const { locale } = await params;  // Resuelve la promesa para obtener el valor de `locale`
 
   if (!routing.locales.includes(locale)) {
     notFound();
